f6c6d5d682e354e04c512b0216c8c66787bbab48
shyn <shynaz87@gmail.com>
2026-06-11 02:08:27 -0700

    feat: setup Prisma service with MariaDB adapter

---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,10 @@
+// src/prisma/prisma.module.ts
+import { Global, Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Global() // گلوبال کردن باعث می‌شود نیاز نباشد همه‌جا ایمپورتش کنید
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+export class PrismaModule {}
