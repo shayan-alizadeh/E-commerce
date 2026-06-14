8c8e2ee71824e51624227714795d7e797bdbd00a
shyn <shynaz87@gmail.com>
2026-06-14 04:56:18 -0700

    feat(main): configure global validation pipe for DTOs

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -12,25 +12,25 @@ import {
 import { roleType } from '../../../generated/prisma/enums.js';
 
 export class CreateUserDto {
-//   @ApiProperty({ example: '09125806033' })
+  //   @ApiProperty({ example: '09125806033' })
   @IsString()
   @IsNotEmpty()
   @Matches(/^.{11}$/)
   @Transform(({ value }) => value.trim())
   mobile!: string;
 
-//   @ApiProperty({ example: 'Ali' })
+  //   @ApiProperty({ example: 'Ali' })
   @IsString()
   @IsNotEmpty()
   display_name!: string;
 
-//   @ApiPropertyOptional({ example: '123456' })
+  //   @ApiPropertyOptional({ example: '123456' })
   @IsString()
-  @IsOptional()
+  @IsNotEmpty()
   @Length(8, 16)
-  password?: string;
+  password!: string;
 
-//   @ApiPropertyOptional({ enum: roleType, example: roleType.user })
+  //   @ApiPropertyOptional({ enum: roleType, example: roleType.user })
   @IsOptional()
   @IsEnum(roleType)
   role?: roleType;
