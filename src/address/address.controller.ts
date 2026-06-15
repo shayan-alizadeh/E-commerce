c9c39abda8c203ebe6000cedb175bd8a51e6f6c5
shyn <shynaz87@gmail.com>
2026-06-15 11:06:54 -0700

    feat: complete address.controller

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,34 +1,87 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Res,
+  HttpStatus,
+  Put,
+} from '@nestjs/common';
 import { AddressService } from './address.service.js';
 import { CreateAddressDto } from './dto/create-address.dto.js';
 import { UpdateAddressDto } from './dto/update-address.dto.js';
+import type { Response } from 'express';
+// import { Permission } from 'src/auth/Decorator/permission.decorator';
+// import { ApiBearerAuth } from '@nestjs/swagger';
 
+// @ApiBearerAuth()
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
   @Post()
-  create(@Body() createAddressDto: CreateAddressDto) {
-    return this.addressService.create(createAddressDto);
+  async create(
+    @Body() createAddressDto: CreateAddressDto,
+    @Res() res: Response,
+  ) {
+    const result = await this.addressService.create(createAddressDto);
+    return res.status(HttpStatus.CREATED).json({
+      success: true,
+      body: result,
+      message: ` Address Created `,
+      status: HttpStatus.CREATED,
+    });
   }
 
   @Get()
-  findAll() {
-    return this.addressService.findAll();
+  async findAll(@Res() res: Response) {
+    const result = await this.addressService.findAll();
+    return res.status(HttpStatus.OK).json({
+      success: true,
+      body: result,
+      message: ` Addresses Founded `,
+      status: HttpStatus.OK,
+    });
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.addressService.findOne(+id);
+  async findOne(@Param('id') id: string, @Res() res: Response) {
+    const result = await this.addressService.findOne(+id);
+    return res.status(HttpStatus.OK).json({
+      success: true,
+      body: result,
+      message: ` Address ${id} Founded `,
+      status: HttpStatus.OK,
+    });
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAddressDto: UpdateAddressDto) {
-    return this.addressService.update(+id, updateAddressDto);
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateAddressDto: UpdateAddressDto,
+    @Res() res: Response,
+  ) {
+    const result = await this.addressService.update(+id, updateAddressDto);
+    return res.status(HttpStatus.OK).json({
+      success: true,
+      body: result,
+      message: ` Address ${id} Updated `,
+      status: HttpStatus.OK,
+    });
   }
 
+  //این دکوراتور مربوط به بخش است که بعدا اضافه شده (ownership Guard)
+  // @Permission('address:delete:own')
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.addressService.remove(+id);
+  async remove(@Param('id') id: string, @Res() res: Response) {
+    const result = await this.addressService.remove(+id);
+    return res.status(HttpStatus.OK).json({
+      success: true,
+      message: ` Address ${id} Deleted `,
+      status: HttpStatus.OK,
+    });
   }
 }
