19729f8cd145de7551f3a923585004c9f37b2bc2
shyn <shynaz87@gmail.com>
2026-06-15 23:59:58 -0700

    feat(ticket): create-ticket.dto and update-ticket.dto

--- src/ticket/dto/create-ticket.dto.ts
+++ src/ticket/dto/create-ticket.dto.ts
@@ -1,1 +1,21 @@
-export class CreateTicketDto {}
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateTicketDto {
+  @IsString({ message: 'توضیحات باید به صورت رشته باشد' })
+  @IsNotEmpty({ message: 'توضیحات نباید خالی باشد' })
+  title!: string;
+
+  @IsString({ message: 'توضیحات باید به صورت رشته باشد' })
+  @IsNotEmpty({ message: 'توضیحات نباید خالی باشد' })
+  subject!: string;
+
+  @IsString({ message: 'توضیحات باید به صورت رشته باشد' })
+  @IsNotEmpty({ message: 'توضیحات نباید خالی باشد' })
+  description!: string;
+
+  @IsNotEmpty({ message: 'id کاربر نمی تواند خالی باشد .' })
+  userId!: number;
+
+  @IsOptional()
+  replyId?: number;
+}
--- src/ticket/dto/update-ticket.dto.ts
+++ src/ticket/dto/update-ticket.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateTicketDto } from './create-ticket.dto';
+import { CreateTicketDto } from './create-ticket.dto.js';
 
 export class UpdateTicketDto extends PartialType(CreateTicketDto) {}
