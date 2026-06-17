6a9a0777885fb8e5d8aeb7a653c4a0ef5f69ce68
shyn <shynaz87@gmail.com>
2026-06-17 00:19:14 -0700

    feat: implement validation for product and category DTOs

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,1 +1,19 @@
-export class CreateProductDto {}
+import { IsString, IsInt, IsOptional, IsArray } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  title!: string;
+
+  @IsInt()
+  price!: number;
+
+  @IsString()
+  description!: string;
+
+  @IsInt()
+  stock!: number;
+
+  @IsOptional()
+  @IsArray()
+  categoryIds?: number[];
+}
