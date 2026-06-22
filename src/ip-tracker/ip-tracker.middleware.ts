1a1af76c72f701da75fef32978e1c2842759a54b
shyn <shynaz87@gmail.com>
2026-06-22 12:36:52 -0700

    feat:complete ip-tracker.middleware

--- src/ip-tracker/ip-tracker.middleware.ts
+++ src/ip-tracker/ip-tracker.middleware.ts
@@ -1,9 +1,13 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { IpTrackerService } from './ip-tracker.service.js';
 
 @Injectable()
 export class IpTrackerMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: () => void) {
+  constructor(private readonly ipTrackerService: IpTrackerService) {}
+  async use(req: Request, res: Response, next: () => void) {
+    const clientIp = req.ip || 'Unknown IP';
+    await this.ipTrackerService.track(clientIp);
     next();
   }
 }
