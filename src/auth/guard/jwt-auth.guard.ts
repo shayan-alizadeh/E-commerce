47b8e0c989a2374e28e84595aff683b4cbadbc8d
shyn <shynaz87@gmail.com>
2026-06-27 22:50:49 -0700

    feat:complete jwt auth guard

---
+++ src/auth/guard/jwt-auth.guard.ts
@@ -0,0 +1,38 @@
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+import { IS_PUBLIC_KEY } from '../Decorator/public.decorator';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride(IS_PUBLIC_KEY, [
+      context.getClass(),
+      context.getHandler(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+    return super.canActivate(context);
+  }
+  handleRequest(err: any, user: any, info: Error, context: ExecutionContext) {
+    if (err || !user) {
+      throw err || new UnauthorizedException('توکن شما نامعتبر است .');
+    }
+
+    console.log(user);
+    console.log('--------------------------');
+    console.log(info);
+    console.log('--------------------------');
+    console.log(context);
+    console.log('--------------------------');
+    return user;
+  }
+}
