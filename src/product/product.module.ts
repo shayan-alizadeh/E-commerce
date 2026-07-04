8b0e2a75a44c73c30e57341cf871a0bee17abf64
shyn <shynaz87@gmail.com>
2026-07-04 00:34:50 -0700

    fix : resolve product module error

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ProductService } from './product.service';
-import { ProductController } from './product.controller';
+import { ProductService } from './product.service.js';
+import { ProductController } from './product.controller.js';
 
 @Module({
   controllers: [ProductController],
